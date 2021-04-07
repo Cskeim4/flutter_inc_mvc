c836d93fe57de8387820f42dd4a79b1b4b30d887
c.skeim <cskeim@css.edu>
2021-04-07 16:51:33 -0500

    Finished Version

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,25 +1,30 @@
-
 import 'package:flutter_inc_mvc/model.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
 
 //Controller interacts directly with the model and the view, it sits between them
+//The controller is bound to the view so the increment/decrement functions work
 
+//Our controller extends the MVC controller class
 class Controller extends ControllerMVC {
   factory Controller(){
     if(_this == null) _this = Controller._();
     return _this;
   }
 
+  //Initialize controller
   static Controller _this;
 
   Controller._();
 
   int get counter => Model.counter;
 
+  //Method to increment the counter
   void incrementCounter(){
     //Controller communicates with the model
     Model.incrementCounter();
   }
 
+  //Method to decrement the counter
   void decrementCounter(){
     //Controller communicates with the model, controller knows the name, model does the work
     Model.decrementCounter();
--- lib/model.dart
+++ lib/model.dart
@@ -2,10 +2,12 @@
 //Interacts with the view indirectly
 
 class Model{
+  //Initialize variables
   static int _counter = 0;
   static int get counter => _counter;
 
+  //Initialize counters
   static int incrementCounter() => ++_counter;
-  static int decrementCounter() => --_counter;
+  static int decrementCounter() => _counter--;
 
 }
